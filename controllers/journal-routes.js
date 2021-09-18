b71f1ad2e9128a192ba54dc148079b3838648ea2
lucawic <lucaswick@email.arizona.edu>
2021-09-18 10:24:22 -0700

    added routes to journal page

--- controllers/journal-routes.js
+++ controllers/journal-routes.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
-});
-
-router.get('/journal', (req, res) => {
-  res.render('journal');
+    res.render('journal');
 });
 
 module.exports = router;
