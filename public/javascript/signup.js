36dff2e644704b0288d17d52c4421cf8683f9c12
Your Name <jasjohn3@uat.edu>
2021-09-19 11:28:17 -0700

    Signup form successfull adds a new user to the database.  Unhandled promise error still exists

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -24,4 +24,4 @@ async function signupFormHandler(event) {
   }
 }
 
-document.querySelector('signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
