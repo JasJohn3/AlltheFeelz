d25c4516dc062df00563cdbdfbd746b67201eda0
walidhkhan <walidhkhan@gmail.com>
2021-09-21 05:05:03 -0700

    updated posts.handlebars layout & added helper functions to handlebars.js

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
