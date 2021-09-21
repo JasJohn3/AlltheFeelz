4a53be044bb9a28f6509a2d3d869cad671d2b36e
walidhkhan <walidhkhan@gmail.com>
2021-09-21 14:19:34 -0700

    formatted date to month, day, year

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
 const session = require('express-session');
-const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -18,6 +16,10 @@ const sess = {
     db: sequelize
   })
 };
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.engine('handlebars',hbs.engine);
