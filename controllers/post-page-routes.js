81a9f868700aa8e1a095443f62812a63724150c5
walidhkhan <walidhkhan@gmail.com>
2021-09-21 01:02:19 -0700

    added functionality to display all posts to http://localhost:3001/posts/all

--- controllers/post-page-routes.js
+++ controllers/post-page-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
 // sessionkey from utils folder
-// const tempAuth = 
+// const tempAuth = require('../')
 
 // get all posts for http://localhost:3001/posts/ ----------------------
-router.get('/', (req, res) => {
+router.get('/all', (req, res) => {
     Post.findAll({
         attributes: [
             'id',
@@ -16,8 +16,8 @@ router.get('/', (req, res) => {
             {
                 model: User,
                 attributes: ['username']
-            },
-        ],
+            }
+        ]
     }).then(postdata => {
         const posts = postdata.map((post) => post.get({ plain: true }));
         res.render('posts', {
@@ -39,6 +39,12 @@ router.get('/:id', (req, res) => {
             // message: req.params.message,
             // user_id: req.params.user_id
         },
+        attributes: [
+            'id',
+            'title',
+            'message',
+            'created_at'
+        ],
         include: [
             {
                 model: User,
