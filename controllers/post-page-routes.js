012cc1c7afc32cc661edf7afc6fdcb85780207eb
walidhkhan <walidhkhan@gmail.com>
2021-09-20 19:27:23 -0700

    testing all posts

--- controllers/post-page-routes.js
+++ controllers/post-page-routes.js
@@ -1,20 +1,61 @@
 const router = require('express').Router();
-const { Post } = require('../models/Post');
+const { Post, User } = require('../models');
 // sessionkey from utils folder
 // const tempAuth = 
 
+// get all posts for http://localhost:3001/posts/ ----------------------
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'message',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+        ],
+    }).then(postdata => {
+        const posts = postdata.map((post) => post.get({ plain: true }));
+        res.render('allposts', {
+            layout: "main",
+            posts
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+// original (working with http://localhost:3001/posts/id)
 router.get('/:id', (req, res) => {
     Post.findAll({
         where: {
-            id: req.params.id
-        }
+            id: req.params.id,
+            // title: req.params.title,
+            // message: req.params.message,
+            // user_id: req.params.user_id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+        ],
     }).then(postdata => {
         const posts = postdata.map((post) => post.get({ plain: true }));
-        res.render('allposts',{
-            layout: "postspage",
+        res.render('allposts', {
+            layout: "main",
             posts
         })
-    }).catch(err => {console.log(err)});
-})
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
 
 module.exports = router
