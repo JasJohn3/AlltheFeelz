ca8cb64c9be66e3ca2ad5647a353297c0f8458fc
lucawic <lucaswick@email.arizona.edu>
2021-09-21 19:52:41 -0700

    journal out of date

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Category } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('journal');
 });
 
 router.get('/journal', (req, res) => {
@@ -14,6 +17,140 @@ router.get('/login', (req, res) => {
     return;
   }
 
+
+  router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        console.log(posts);
+        Category.findAll({}).then(dbCatData => {
+          const categories = dbCatData.map(post => post.get({ plain: true }))
+          console.log(categories)
+          console.log(posts)
+          res.render('journal', {
+            categories,
+            posts,
+            loggedIn: req.session.loggedIn
+          });
+        })
+  
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  
+  
+  });
+  
+  router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        const post = dbPostData.get({ plain: true });
+  
+        res.render('single-post', {
+          post,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (dbPostData) {
+          const post = dbPostData.get({ plain: true });
+          
+          res.render('edit-post', {
+            post,
+            loggedIn: true
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+
   res.render('login');
 });
 
@@ -22,3 +159,5 @@ router.get('/signup',(req,res)=>{
 });
 
 module.exports = router;
+
+
--- controllers/journal-routes.js
+++ controllers/journal-routes.js
@@ -1,7 +1,54 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Category } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-    res.render('journal');
+router.get('/', withAuth, (req, res) => {
+  console.log(req.session);
+  console.log('======================');
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      Category.findAll({}).then(dbCatData => {
+        const categories = dbCatData.map(post => post.get({ plain: true}))
+        console.log(categories)
+        res.render('journal', {
+          categories,
+          posts,
+          loggedIn: req.session.loggedIn
+        })
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
+
 
 module.exports = router;
